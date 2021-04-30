5aac82577720a28cfc9b8282cf9ac1bf450fcf1d
jokale <kalemajoanna@gmail.com>
2021-04-30 11:31:35 +0100

    create carbon component

---
+++ src/Components/Carbon.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Carbon extends Component {
+    state = { 
+        data = []
+     }
+    render() { 
+        return ( <div>
+            <h1> I am the Carbon data </h1>
+        </div> );
+    }
+}
+ 
+export default Carbon;
