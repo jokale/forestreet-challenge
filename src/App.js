1a20ff9d040cf77f255414df087642fd501e4ab1
jokale <kalemajoanna@gmail.com>
2021-04-30 16:17:09 +0100

    build fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Carbon from './Components/Carbon';
 
--- src/Components/Carbon.js
+++ src/Components/Carbon.js
@@ -4,9 +4,28 @@ class Carbon extends Component {
     state = { 
         data: []
      }
+
+     retrieveCarbon = (e) => {
+         e.preventDefault();
+         fetch('https://api.carbonintensity.org.uk/intensity')
+        //  .then(res => res.text())          
+        //  .then(text => console.log(text))
+         .then(response => response.json())
+         .then(console.log("hello i have fetched1",))
+         .then(carbonData =>
+           this.setState({
+               data: carbonData
+             })
+         )
+         .then(console.log("hello i have fetched2"))
+     }        
+
+
     render() { 
         return ( <div>
             <h1> I am the Carbon data </h1>
+            <button onClick={this.retrieveCarbon}></button>
+            <h5>{this.state.data.data.intensity}</h5>
         </div> );
     }
 }
